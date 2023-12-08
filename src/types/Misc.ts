58203e613b71aebe7d229f76121388368bf9ad77
Jono Luiz Cosio <jonoluiz@gmail.com>
2023-12-08 16:56:53 +0800

    Add basic types

---
+++ src/types/Misc.ts
@@ -0,0 +1,26 @@
+export enum Affinities {
+    Resistance = 'rs',
+    Vulnerability = 'vu',
+    Absorpbtion = 'ab',
+    None = 'no'
+}
+
+export enum Attributes {
+    Might = 'might',
+    Dexterity = 'dexterity',
+    Insight = 'insight',
+    Willpower = 'will'
+}
+
+export enum Elements {
+    Physical = 'physical',
+    Wind = 'wind',
+    Bolt = 'bolt',
+    Dark = 'dark',
+    Earth = 'earth',
+    Fire = 'fire',
+    Ice = 'ice',
+    Light = 'light',
+    Poison = 'poison'
+}
+
