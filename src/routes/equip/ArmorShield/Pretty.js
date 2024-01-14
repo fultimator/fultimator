9b3b9c5db59678150bfe469231b2c34fb5959c0c
greg-argulla <greg.argulla@gmail.com>
2024-01-14 13:28:57 +0800

    fix: add download image and export json for items

--- src/routes/equip/ArmorShield/Pretty.js
+++ src/routes/equip/ArmorShield/Pretty.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { createRef, useRef, useState, useEffect } from "react";
 import {
   Card,
   Grid,
@@ -7,9 +7,13 @@ import {
   Box,
   Avatar,
   useTheme,
-  ThemeProvider
+  ThemeProvider,
+  Tooltip,
+  IconButton,
 } from "@mui/material";
 import ArrowDownward from "@mui/icons-material/ArrowDownward";
+import { createFileName, useScreenshot } from "use-react-screenshot";
+import { Download, Code } from "@mui/icons-material";
 
 function Pretty({ base, custom }) {
   const theme = useTheme();
@@ -20,13 +24,14 @@ function Pretty({ base, custom }) {
         <Typography textAlign="center">
           <ArrowDownward />
         </Typography>
-        <PrettySingle armor={custom} />
+        <PrettySingle armor={custom} showActions />
       </div>
     </ThemeProvider>
   );
 }
 
-function PrettySingle({ armor }) {
+function PrettySingle({ armor, showActions }) {
+  const ref = createRef(null);
   const theme = useTheme();
   const primary = theme.palette.primary.main;
   const secondary = theme.palette.secondary.main;
@@ -48,144 +53,203 @@ function PrettySingle({ armor }) {
     reader.readAsDataURL(selectedFile);
   };
 
-  return (
-    <Card>
-      <Stack>
-        <Grid
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{
-            p: 1,
-            background: `${primary}`,
-            color: "#ffffff",
-            "& .MuiTypography-root": {
-              fontSize: "1.2rem",
-              textTransform: "uppercase",
-            },
-          }}
-        >
-          <Grid item xs={1}></Grid>
-          <Grid item xs={3}>
-            <Typography variant="h4" textAlign="left">
-              {armor.category}
-            </Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography variant="h4" textAlign="center">
-              Cost
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="h4" textAlign="center">
-              Defense
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h4" textAlign="center">
-              M. Defense
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container>
-          {/* Image */}
-          <Grid
-            item
-            xs={2}
-            sx={{
-              flex: "0 0 70px",
-              minWidth: "70px",
-              minHeight: "70px",
-              overflow: "hidden",
-              border: `1px solid ${primary}`,
-              background: `${ternary}`,
-            }}
-            onClick={handleGridItemClick}
-          >
-            <Box
-              sx={{
-                width: "70px",
-                height: "70px",
-                background: "white",
-                border: `1px solid ${primary}`,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-              }}
-            >
-              <Avatar
-                alt="Image"
-                src={selectedImage || ""}
-                sx={{ objectFit: "cover", borderRadius: "0" }}
-              />
-            </Box>
-            <input
-              type="file"
-              ref={fileInputRef}
-              style={{ display: "none" }}
-              onChange={handleFileInputChange}
-            />
-          </Grid>
+  const [image, takeScreenShot] = useScreenshot();
 
-          <Grid container direction="column" item xs>
-            {/* First Row */}
+  function downloadFile(content, fileName, contentType) {
+    const a = document.createElement("a");
+    const file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+  const getJSON = () => {
+    const jsonData = JSON.stringify(armor);
+    const fileName = `${armor.name.replace(/\s/g, "_").toLowerCase()}.json`;
+    downloadFile(jsonData, fileName, "text/plain");
+  };
+
+  const download = (image, { name = "img", extension = "png" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const getImage = () => {
+    takeScreenShot(ref.current);
+  };
+
+  useEffect(() => {
+    if (image) {
+      download(image, { name: armor.name, extension: "png" });
+    }
+  }, [image, armor.name]);
+
+  return (
+    <>
+      <Card>
+        <div
+          ref={ref}
+          style={{ backgroundColor: "white", background: "white" }}
+        >
+          <Stack>
             <Grid
               container
               justifyContent="space-between"
-              item
+              alignItems="center"
               sx={{
-                background: `linear-gradient(to right, ${ternary}, transparent)`,
-                borderBottom: `1px solid ${secondary}`,
-                padding: "5px",
+                p: 1,
+                background: `${primary}`,
+                color: "#ffffff",
+                "& .MuiTypography-root": {
+                  fontSize: "1.2rem",
+                  textTransform: "uppercase",
+                },
               }}
             >
+              <Grid item xs={1}></Grid>
               <Grid item xs={3}>
-                <Typography fontWeight="bold">{armor.name}</Typography>
+                <Typography variant="h4" textAlign="left">
+                  {armor.category}
+                </Typography>
               </Grid>
               <Grid item xs={1}>
-                <Typography textAlign="center">{`${armor.cost}z`}</Typography>
+                <Typography variant="h4" textAlign="center">
+                  Cost
+                </Typography>
               </Grid>
-              <Grid item xs={4}>
-                <Typography fontWeight="bold" textAlign="center">
-                  {armor.category === "Shield" ? "+" + armor.def : ""}
-                  {armor.category === "Armor" && armor.martial ? armor.def : ""}
-                  {armor.category === "Armor" && !armor.martial
-                    ? armor.def === 0
-                      ? "DEX die"
-                      : "DEX die +" + armor.def
-                    : ""}
+              <Grid item xs={3}>
+                <Typography variant="h4" textAlign="center">
+                  Defense
                 </Typography>
               </Grid>
               <Grid item xs={4}>
-                <Typography fontWeight="bold" textAlign="center">
-                  {armor.category === "Shield" ? "+" + armor.mdef : ""}
-                  {armor.category === "Armor" && armor.martial ? armor.def : ""}
-                  {armor.category === "Armor"
-                    ? armor.mdef === 0
-                      ? "INS die"
-                      : "INS die +" + armor.mdef
-                    : ""}
+                <Typography variant="h4" textAlign="center">
+                  M. Defense
                 </Typography>
               </Grid>
             </Grid>
+            <Grid container>
+              {/* Image */}
+              <Grid
+                item
+                xs={2}
+                sx={{
+                  flex: "0 0 70px",
+                  minWidth: "70px",
+                  minHeight: "70px",
+                  overflow: "hidden",
+                  border: `1px solid ${primary}`,
+                  background: `${ternary}`,
+                }}
+                onClick={handleGridItemClick}
+              >
+                <Box
+                  sx={{
+                    width: "70px",
+                    height: "70px",
+                    background: "white",
+                    border: `1px solid ${primary}`,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Avatar
+                    alt="Image"
+                    src={selectedImage || ""}
+                    sx={{ objectFit: "cover", borderRadius: "0" }}
+                  />
+                </Box>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileInputChange}
+                />
+              </Grid>
 
-            {/* Second Row */}
-            <Grid
-              container
-              justifyContent="flex-start"
-              sx={{
-                background: "transparent",
-                borderBottom: `1px solid ${secondary}`,
-                padding: "5px",
-              }}
-            >
-              {!armor.quality && "No Qualities"} {armor.quality}
+              <Grid container direction="column" item xs>
+                {/* First Row */}
+                <Grid
+                  container
+                  justifyContent="space-between"
+                  item
+                  sx={{
+                    background: `linear-gradient(to right, ${ternary}, transparent)`,
+                    borderBottom: `1px solid ${secondary}`,
+                    padding: "5px",
+                  }}
+                >
+                  <Grid item xs={3}>
+                    <Typography fontWeight="bold">{armor.name}</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Typography textAlign="center">{`${armor.cost}z`}</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography fontWeight="bold" textAlign="center">
+                      {armor.category === "Shield" ? "+" + armor.def : ""}
+                      {armor.category === "Armor" && armor.martial
+                        ? armor.def
+                        : ""}
+                      {armor.category === "Armor" && !armor.martial
+                        ? armor.def === 0
+                          ? "DEX die"
+                          : "DEX die +" + armor.def
+                        : ""}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography fontWeight="bold" textAlign="center">
+                      {armor.category === "Shield" ? "+" + armor.mdef : ""}
+                      {armor.category === "Armor" && armor.martial
+                        ? armor.def
+                        : ""}
+                      {armor.category === "Armor"
+                        ? armor.mdef === 0
+                          ? "INS die"
+                          : "INS die +" + armor.mdef
+                        : ""}
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                {/* Second Row */}
+                <Grid
+                  container
+                  justifyContent="flex-start"
+                  sx={{
+                    background: "transparent",
+                    padding: "5px",
+                  }}
+                >
+                  <Typography>
+                    {!armor.quality && "No Qualities"} {armor.quality}
+                  </Typography>
+                </Grid>
+              </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </Stack>
-    </Card>
+          </Stack>
+        </div>
+      </Card>
+      {showActions && (
+        <div style={{ display: "flex" }}>
+          <Tooltip title="Download">
+            <IconButton onClick={getImage}>
+              <Download />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Export JSON">
+            <IconButton onClick={getJSON}>
+              <Code />
+            </IconButton>
+          </Tooltip>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/routes/equip/weapons/Pretty.js
+++ src/routes/equip/weapons/Pretty.js
@@ -1,11 +1,25 @@
-import React, { useRef, useState } from 'react';
-import { Card, Grid, Stack, Typography, Box, Avatar, useTheme, ThemeProvider} from "@mui/material";
+import React, { useRef, useState, useEffect, createRef } from "react";
+import {
+  Card,
+  Grid,
+  Stack,
+  Typography,
+  Box,
+  Avatar,
+  useTheme,
+  ThemeProvider,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
 import ArrowDownward from "@mui/icons-material/ArrowDownward";
 import ReactMarkdown from "react-markdown";
 import attributes from "../../../libs/attributes";
 import types from "../../../libs/types";
-import { OpenBracket, CloseBracket } from "../../../components/Bracket"
+import { OpenBracket, CloseBracket } from "../../../components/Bracket";
 import Diamond from "../../../components/Diamond";
+
+import { createFileName, useScreenshot } from "use-react-screenshot";
+import { Download, Code } from "@mui/icons-material";
 
 function Pretty({ base, custom }) {
   const theme = useTheme();
@@ -18,13 +32,14 @@ function Pretty({ base, custom }) {
         <Typography textAlign="center">
           <ArrowDownward />
         </Typography>
-        <PrettySingle weapon={custom} />
+        <PrettySingle weapon={custom} showActions />
       </div>
     </ThemeProvider>
   );
 }
 
-function PrettySingle({ weapon }) {
+function PrettySingle({ weapon, showActions }) {
+  const ref = createRef(null);
   const theme = useTheme();
   const primary = theme.palette.primary.main;
   const secondary = theme.palette.secondary.main;
@@ -46,151 +61,231 @@ function PrettySingle({ weapon }) {
     reader.readAsDataURL(selectedFile);
   };
 
-  return (
-    <Card>
-      <Stack>
-        <Grid
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{
-            p: 1,
-            background: `${primary}`,
-            color: "#ffffff",
-            "& .MuiTypography-root": {
-              fontSize: "1.2rem",
-              textTransform: "uppercase",
-            },
-          }}
-        >
-          <Grid item xs={1}></Grid>
-          <Grid item xs={3}>
-            <Typography variant="h4" textAlign="left">
-              Weapon
-            </Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography variant="h4" textAlign="center">
-              Cost
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="h4" textAlign="center">
-              Accuracy
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h4" textAlign="center">
-              Damage
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container>
-        {/* Image */}
-        <Grid item xs={2} sx={{ flex: "0 0 70px", minWidth: "70px", minHeight: "70px", overflow: "hidden", border: `1px solid ${primary}`, background: `${ternary}` }} onClick={handleGridItemClick}>
-          <Box
-            sx={{
-              width: "70px",
-              height: "70px",
-              background: "white",
-              border: `1px solid ${primary}`,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "pointer",
-            }}
-          >
-            <Avatar alt="Image" src={selectedImage || ""} sx={{ objectFit: "cover", borderRadius: "0" }} />
-          </Box>
-          <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleFileInputChange} />
-        </Grid>
+  const [image, takeScreenShot] = useScreenshot();
 
-          <Grid container direction="column" item xs>
-            {/* First Row */}
+  function downloadFile(content, fileName, contentType) {
+    const a = document.createElement("a");
+    const file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+  const getJSON = () => {
+    const jsonData = JSON.stringify(weapon);
+    const fileName = `${weapon.name.replace(/\s/g, "_").toLowerCase()}.json`;
+    downloadFile(jsonData, fileName, "text/plain");
+  };
+
+  const download = (image, { name = "img", extension = "png" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const getImage = () => {
+    takeScreenShot(ref.current);
+  };
+
+  useEffect(() => {
+    if (image) {
+      download(image, { name: weapon.name, extension: "png" });
+    }
+  }, [image, weapon.name]);
+
+  return (
+    <>
+      <Card>
+        <div
+          ref={ref}
+          style={{ backgroundColor: "white", background: "white" }}
+        >
+          <Stack>
             <Grid
               container
               justifyContent="space-between"
-              item
+              alignItems="center"
               sx={{
-                background: `linear-gradient(to right, ${ternary}, transparent)`,
-                borderBottom: `1px solid ${secondary}`,
-                padding: '5px',
+                p: 1,
+                background: `${primary}`,
+                color: "#ffffff",
+                "& .MuiTypography-root": {
+                  fontSize: "1.2rem",
+                  textTransform: "uppercase",
+                },
               }}
             >
+              <Grid item xs={1}></Grid>
               <Grid item xs={3}>
-                <Typography fontWeight="bold">{weapon.name}</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography textAlign="center">{`${weapon.cost}z`}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography fontWeight="bold" textAlign="center">
-                  <OpenBracket />
-                  {`${attributes[weapon.att1].shortcaps} + ${attributes[weapon.att2].shortcaps}`}
-                  <CloseBracket />
-                  {weapon.prec !== 0 ? `+${weapon.prec}` : ""}
+                <Typography variant="h4" textAlign="left">
+                  Weapon
                 </Typography>
               </Grid>
-
+              <Grid item xs={1}>
+                <Typography variant="h4" textAlign="center">
+                  Cost
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="h4" textAlign="center">
+                  Accuracy
+                </Typography>
+              </Grid>
               <Grid item xs={4}>
-                <Typography fontWeight="bold" textAlign="center">
-                  <OpenBracket />
-                  HR + {weapon.damage}
-                  <CloseBracket />
-                  {types[weapon.type].long}
+                <Typography variant="h4" textAlign="center">
+                  Damage
                 </Typography>
               </Grid>
             </Grid>
+            <Grid container>
+              {/* Image */}
+              <Grid
+                item
+                xs={2}
+                sx={{
+                  flex: "0 0 70px",
+                  minWidth: "70px",
+                  minHeight: "70px",
+                  overflow: "hidden",
+                  border: `1px solid ${primary}`,
+                  background: `${ternary}`,
+                }}
+                onClick={handleGridItemClick}
+              >
+                <Box
+                  sx={{
+                    width: "70px",
+                    height: "70px",
+                    background: "white",
+                    border: `1px solid ${primary}`,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Avatar
+                    alt="Image"
+                    src={selectedImage || ""}
+                    sx={{ objectFit: "cover", borderRadius: "0" }}
+                  />
+                </Box>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileInputChange}
+                />
+              </Grid>
 
-            {/* Second Row */}
-            <Grid
-              container
-              justifyContent="flex-end"
+              <Grid container direction="column" item xs>
+                {/* First Row */}
+                <Grid
+                  container
+                  justifyContent="space-between"
+                  item
+                  sx={{
+                    background: `linear-gradient(to right, ${ternary}, transparent)`,
+                    borderBottom: `1px solid ${secondary}`,
+                    padding: "5px",
+                  }}
+                >
+                  <Grid item xs={3}>
+                    <Typography fontWeight="bold">{weapon.name}</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Typography textAlign="center">{`${weapon.cost}z`}</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography fontWeight="bold" textAlign="center">
+                      <OpenBracket />
+                      {`${attributes[weapon.att1].shortcaps} + ${
+                        attributes[weapon.att2].shortcaps
+                      }`}
+                      <CloseBracket />
+                      {weapon.prec !== 0 ? `+${weapon.prec}` : ""}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <Typography fontWeight="bold" textAlign="center">
+                      <OpenBracket />
+                      HR + {weapon.damage}
+                      <CloseBracket />
+                      {types[weapon.type].long}
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                {/* Second Row */}
+                <Grid
+                  container
+                  justifyContent="flex-end"
+                  sx={{
+                    background: "transparent",
+                    borderBottom: `1px solid ${secondary}`,
+                    padding: "5px",
+                  }}
+                >
+                  <Grid item xs={3}>
+                    <Typography fontWeight="bold">{weapon.category}</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Diamond color={primary} />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography textAlign="center">
+                      {weapon.hands === 1 && "One-handed"}
+                      {weapon.hands === 2 && "Two-handed"}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Diamond color="{primary}" />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography textAlign="center">
+                      {weapon.melee && "Melee"}
+                      {weapon.ranged && "Ranged"}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Typography
               sx={{
                 background: "transparent",
                 borderBottom: `1px solid ${secondary}`,
-                padding: '5px',
+                px: 1,
+                py: 1,
               }}
             >
-              <Grid item xs={3}>
-                <Typography fontWeight="bold">{weapon.category}</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Diamond color={primary} />
-              </Grid>
-              <Grid item xs={4}>
-                <Typography textAlign="center">
-                  {weapon.hands === 1 && "One-handed"}
-                  {weapon.hands === 2 && "Two-handed"}
-                </Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Diamond color="{primary}" />
-              </Grid>
-              <Grid item xs={3}>
-                <Typography textAlign="center">
-                  {weapon.melee && "Melee"}
-                  {weapon.ranged && "Ranged"}
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Typography
-          sx={{
-            background: "transparent",
-            borderBottom: `1px solid ${secondary}`,
-            px: 1,
-            py: 1
-          }}
-        >
-          {!weapon.quality && "No Qualities"}{" "}
-          <ReactMarkdown allowedElements={["strong"]} unwrapDisallowed={true}>
-            {weapon.quality}
-          </ReactMarkdown>
-        </Typography>
-      </Stack>
-    </Card>
+              {!weapon.quality && "No Qualities"}{" "}
+              <ReactMarkdown
+                allowedElements={["strong"]}
+                unwrapDisallowed={true}
+              >
+                {weapon.quality}
+              </ReactMarkdown>
+            </Typography>
+          </Stack>
+        </div>
+      </Card>
+      {showActions && (
+        <div style={{ display: "flex" }}>
+          <Tooltip title="Download">
+            <IconButton onClick={getImage}>
+              <Download />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Export JSON">
+            <IconButton onClick={getJSON}>
+              <Code />
+            </IconButton>
+          </Tooltip>
+        </div>
+      )}
+    </>
   );
 }
 
