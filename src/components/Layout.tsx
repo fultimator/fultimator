b2f7b218132a993869df487f21c1cd3a27007426
Matteo Suppo <matteo.suppo@gmail.com>
2024-01-19 18:24:51 +0100

    Add logo and sublogo in various dimensions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,15 @@ import { Container, Divider, Typography } from "@mui/material";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 
+import logo200 from "./logo_200.webp";
+import logo929 from "./logo_929.webp";
+import logo1400 from "./logo_1400.webp";
+
+import sublogo200 from "./sublogo_200.webp";
+import sublogo834 from "./sublogo_834.webp";
+import sublogo1262 from "./sublogo_1262.webp";
+import sublogo1400 from "./sublogo_1400.webp";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -17,18 +26,24 @@ const Layout = ({ children }: LayoutProps) => {
           textAlign: "center",
         }}
       >
-        <Typography variant="h1">Fultimator!</Typography>
+        <Typography variant="h1" textAlign="center" sx={{p: 0, m:0}}>
+          <img
+            width="60%"
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcSet={`${logo200} 200w,${logo929} 929w,${logo1400} 1400w`}
+            src={logo1400}
+            alt="Fultimator"/>
+        </Typography>
       </RouterLink>
-      <Typography
-        style={{
-          color: "inherit",
-          textDecoration: "none",
-          textAlign: "center",
-        }}
-      >
-        Fultimator is an Unofficial tool for TTRPG Fabula Ultima
+      <Typography variant="h2" textAlign="center" sx={{p: 0, m:0}}>
+        <img
+          width="20%"
+          sizes="(max-width: 1400px) 100vw, 1400px"
+          srcSet={`${sublogo200} 200w, ${sublogo834} 814w, ${sublogo1262} 1227w,${sublogo1400} 1400w`}
+          src={sublogo1400}
+          alt="An Unofficial tool for TTRPG Fabula Ultima"/>
       </Typography>
-      <Divider sx={{ my: 2 }}></Divider>
+      <Divider sx={{my: 2}}></Divider>
       {children}
     </Container>
   );
