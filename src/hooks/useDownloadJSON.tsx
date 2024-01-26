72ff26167698555c3d26e73ddcd88cedc2e100c9
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-01-26 11:00:30 +0800

    Add clipboard copy function to hook

--- src/hooks/useDownloadJSON.tsx
+++ src/hooks/useDownloadJSON.tsx
@@ -1,16 +1,23 @@
 import useDownload from "./useDownload";
 
 // useDownloadJSON exposes a function that converts the given data to json and downloads it
-function useDownloadJSON(name: string, data:any) {
+function useDownloadJSON(name: string, data: any) {
   const [download] = useDownload();
 
   function downloadJSON() {
     const jsonData = JSON.stringify(data);
     const file = new Blob([jsonData], { type: "text/json" });
-    download(URL.createObjectURL(file), name + '.json', "text/json");
+    download(URL.createObjectURL(file), name + ".json", "text/json");
   }
 
-  return [downloadJSON]
+  function copyJSONToClipboard() {
+    const jsonData = JSON.stringify(data);
+    navigator.clipboard.writeText(jsonData).then(() => {
+      console.log("Copy complete", data);
+    });
+  }
+
+  return [downloadJSON, copyJSONToClipboard];
 }
 
 export default useDownloadJSON;
