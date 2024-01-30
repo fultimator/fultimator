ebc498dd7959279a4feddbb32d12576b0934483d
spyrella <16845165+spyrella@users.noreply.github.com>
2024-01-30 06:41:44 -0500

    feat: adopt new root api for react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
@@ -43,8 +43,8 @@ const App = () => {
 };
 
 const root = document.getElementById("root");
-const rootElement = createRoot(root);
-rootElement.render(<App />);
+const rootElement = ReactDOM.createRoot(root);
+rootElement.render(<Root />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
