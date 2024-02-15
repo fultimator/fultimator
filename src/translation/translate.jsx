bfa47c985c3ae40dfbf22088a32ac5c308644b0a
greg-argulla <139342116+gregargulla-i3soft@users.noreply.github.com>
2024-02-15 13:53:18 +0800

    fix: update translations

--- src/translation/translate.jsx
+++ src/translation/translate.jsx
@@ -15,7 +15,7 @@ import data from "./data.json";
     - Paste the whole json in ./translation/data.json
 */
 
-const debugMode = false;
+const debugMode = true;
 
 const translate = (key) => {
   return data.find((item) => item.key === key);
