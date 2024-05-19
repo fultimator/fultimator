b3d34bf0340288601f2668bd9114ead0fc1165c8
Acinoroc <acinoroc@gmail.com>
2024-05-19 11:56:29 +0200

    feat: no damage type base attack

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export function OpenBracket() {
   return (
-    <Typography component="span" sx={{ ml: -1, mr: 0.3 }}>
+    <Typography component="span" sx={{ ml: -1, mr: 0 }}>
       【
     </Typography>
   );
