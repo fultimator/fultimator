19e528fc72334dcf79d2dabba7d56ae0c3e35c9d
Acinoroc <acinoroc@gmail.com>
2024-06-14 15:09:00 +0200

    Player Creator - Spell Roll

--- src/components/player/playerSheet/PlayerSpells.js
+++ src/components/player/playerSheet/PlayerSpells.js
@@ -6,12 +6,22 @@ import {
   IconButton,
   Modal,
   Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Select,
+  MenuItem,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useTranslate } from "../../../translation/translate";
 import { Casino, Info } from "@mui/icons-material";
 import ReactMarkdown from "react-markdown";
 import { OffensiveSpellIcon } from "../../icons";
+import attributes from "../../../libs/attributes";
 
 export default function PlayerSpells({ player, setPlayer }) {
   const { t } = useTranslate();
@@ -22,6 +32,71 @@ export default function PlayerSpells({ player, setPlayer }) {
 
   const [openModal, setOpenModal] = useState(false);
   const [selectedSpell, setSelectedSpell] = useState(null);
+  const [isRolling, setIsRolling] = useState(false);
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState(null);
+  const [targets, setTargets] = useState(1);
+  const [useMp, setUseMp] = useState(true); // New state for MP usage
+
+  const [dialogSeverity, setDialogSeverity] = useState("");
+
+  const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
+  const calculateAttribute = (
+    base,
+    decreaseStatuses,
+    increaseStatuses,
+    min,
+    max
+  ) => {
+    let adjustedValue = base;
+
+    decreaseStatuses.forEach((status) => {
+      if (player.statuses[status]) adjustedValue -= 2;
+    });
+
+    increaseStatuses.forEach((status) => {
+      if (player.statuses[status]) adjustedValue += 2;
+    });
+
+    return clamp(adjustedValue, min, max);
+  };
+
+  const currDex = calculateAttribute(
+    player.attributes.dexterity,
+    ["slow", "enraged"],
+    ["dexUp"],
+    6,
+    12
+  );
+  const currInsight = calculateAttribute(
+    player.attributes.insight,
+    ["dazed", "enraged"],
+    ["insUp"],
+    6,
+    12
+  );
+  const currMight = calculateAttribute(
+    player.attributes.might,
+    ["weak", "poisoned"],
+    ["migUp"],
+    6,
+    12
+  );
+  const currWillpower = calculateAttribute(
+    player.attributes.willpower,
+    ["shaken", "poisoned"],
+    ["wlpUp"],
+    6,
+    12
+  );
+
+  const attributeMap = {
+    dexterity: currDex,
+    insight: currInsight,
+    might: currMight,
+    will: currWillpower,
+  };
 
   /* All spells from all classes */
   const allSpells = player.classes
@@ -41,6 +116,140 @@ export default function PlayerSpells({ player, setPlayer }) {
 
   const handleOK = () => {
     handleCloseModal();
+  };
+
+  const handleRollSetup = (spell) => {
+    setDialogOpen(true);
+    setSelectedSpell(spell);
+    setTargets(1); // Reset targets to 1 when opening the dialog
+    setUseMp(true); // Reset MP usage to true when opening the dialog
+  };
+
+  const handleRoll = () => {
+    if (!isRolling) {
+      const usedMp = selectedSpell.mp * targets;
+
+      if (useMp && player.stats.mp.current < usedMp) {
+        return;
+      } else {
+        const attr1 = selectedSpell.attr1;
+        const attr2 = selectedSpell.attr2;
+
+        let att1Value = attributeMap[attr1];
+        let att2Value = attributeMap[attr2];
+
+        const die1 = Math.floor(Math.random() * att1Value) + 1;
+        const die2 = Math.floor(Math.random() * att2Value) + 1;
+
+        // Check for critical failure
+        const isCriticalFailure = die1 === 1 && die2 === 1;
+        // Check for critical success
+        const isCriticalSuccess = die1 >= 6 && die2 >= 6 && die1 === die2;
+
+        const result = die1 + die2;
+
+        const maxDie = Math.max(die1, die2);
+
+        const usedMp = selectedSpell.mp * targets;
+
+        if (isCriticalFailure) {
+          setDialogSeverity("error");
+        } else if (isCriticalSuccess) {
+          setDialogSeverity("success");
+        } else {
+          setDialogSeverity("");
+        }
+
+        setDialogMessage(
+          <>
+            {" "}
+            {isCriticalSuccess && (
+              <Typography
+                variant="h1"
+                sx={{ textAlign: "center", marginBottom: "10px" }}
+              >
+                Critical Success!
+                <br />
+              </Typography>
+            )}
+            {isCriticalFailure && (
+              <Typography
+                variant="h1"
+                sx={{ textAlign: "center", marginBottom: "10px" }}
+              >
+                Critical Failure!
+                <br />
+              </Typography>
+            )}
+            <Grid container spacing={2} sx={{ textAlign: "center" }}>
+              <Grid item container xs={6}>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="h3"
+                    sx={{ fontWeight: "bold", textTransform: "uppercase" }}
+                  >
+                    {t("Accuracy")}
+                  </Typography>
+                  <Typography variant="h1" sx={{ textAlign: "center" }}>
+                    {result}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item container xs={6}>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="h3"
+                    sx={{ fontWeight: "bold", textTransform: "uppercase" }}
+                  >
+                    {t("HR")}
+                  </Typography>
+                  <Typography variant="h1" sx={{ textAlign: "center" }}>
+                    {maxDie}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} sx={{ marginTop: "20px" }}>
+                <Typography component="span">
+                  {` ${die1} [${attributes[attr1].shortcaps}] + ${die2} [${attributes[attr2].shortcaps}]`}
+                </Typography>
+              </Grid>
+            </Grid>
+          </>
+        );
+
+        /* Remove mp to the player (player.stats.mp.current - usedMp) */
+        if (useMp) {
+          setPlayer((prevPlayer) => {
+            return {
+              ...prevPlayer,
+              stats: {
+                ...prevPlayer.stats,
+                mp: {
+                  ...prevPlayer.stats.mp,
+                  current: prevPlayer.stats.mp.current - usedMp,
+                },
+              },
+            };
+          });
+        }
+
+        setIsRolling(true);
+      }
+    } else {
+      setIsRolling(false);
+      setDialogSeverity("");
+      setDialogMessage("");
+    }
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setSelectedSpell(null);
+    setTargets(1); // Reset targets to 1 when closing the dialog
+    setUseMp(true); // Reset MP usage to true when closing the dialog
+    setIsRolling(false);
+    setDialogSeverity("");
+    setDialogMessage("");
   };
 
   return (
@@ -102,10 +311,15 @@ export default function PlayerSpells({ player, setPlayer }) {
                       width: "100%",
                     }}
                   >
-                    {spell.name}{spell.isOffensive && <OffensiveSpellIcon />}
+                    {spell.name}
+                    {spell.isOffensive && <OffensiveSpellIcon />}
                   </Typography>
                 </Grid>
-                <Grid item xs={2} sx={{ display: "flex", alignItems: "stretch" }}>
+                <Grid
+                  item
+                  xs={2}
+                  sx={{ display: "flex", alignItems: "stretch" }}
+                >
                   <div
                     id="spell-right-controls"
                     style={{
@@ -129,7 +343,7 @@ export default function PlayerSpells({ player, setPlayer }) {
                       <IconButton sx={{ padding: "0px", marginLeft: "5px" }}>
                         <Casino
                           onClick={() => {
-                            alert("Rolls not yet implemented");
+                            handleRollSetup(spell);
                           }}
                         />
                       </IconButton>
@@ -148,7 +362,7 @@ export default function PlayerSpells({ player, setPlayer }) {
             <Paper
               sx={{
                 position: "absolute",
-                width: { xs: "90%", md: 400},
+                width: { xs: "90%", md: 400 },
                 bgcolor: "#fff",
                 border: "2px solid",
                 borderColor: secondary,
@@ -175,6 +389,95 @@ export default function PlayerSpells({ player, setPlayer }) {
               </Button>
             </Paper>
           </Modal>
+          <Dialog
+            open={dialogOpen}
+            onClose={handleDialogClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            PaperProps={{ sx: { width: { xs: "90%", md: "30%" } } }}
+          >
+            <DialogTitle
+              variant="h3"
+              sx={{
+                backgroundColor:
+                  dialogSeverity === "error"
+                    ? "#bb2124"
+                    : dialogSeverity === "success"
+                    ? "#22bb33"
+                    : "#aaaaaa",
+              }}
+              id="alert-dialog-title"
+            >
+              {t("Spell Rolls")}
+            </DialogTitle>
+            <DialogContent sx={{ marginTop: "10px" }}>
+              <DialogContentText id="alert-dialog-description">
+                {!isRolling ? (
+                  <Grid container alignItems="center" spacing={1}>
+                    <Grid item xs={12}>
+                      <Typography variant="body1">
+                        {t("Select number of targets from 1 to")}{" "}
+                        {selectedSpell?.maxTargets || 1}:
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Select
+                        value={targets}
+                        onChange={(e) =>
+                          setTargets(parseInt(e.target.value, 10))
+                        }
+                        size="small"
+                        sx={{ width: "100px" }}
+                      >
+                        {Array.from(
+                          { length: selectedSpell?.maxTargets || 1 },
+                          (_, i) => (
+                            <MenuItem key={i} value={i + 1}>
+                              {i + 1}
+                            </MenuItem>
+                          )
+                        )}
+                      </Select>
+                      {useMp && (
+                        <>
+                          <Typography variant="body1">
+                            {t("MP Cost")}
+                            {": "}
+                            {selectedSpell?.mp * targets}
+                          </Typography>
+                          {selectedSpell?.mp * targets >
+                            player.stats.mp.current && (
+                            <Typography variant="body1" color="error">
+                              {t("Not enough MP")}
+                            </Typography>
+                          )}
+                        </>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={useMp}
+                            onChange={(e) => setUseMp(e.target.checked)}
+                          />
+                        }
+                        label={t("Use MP")}
+                      />
+                    </Grid>
+                  </Grid>
+                ) : (
+                  <>{dialogMessage}</>
+                )}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDialogClose}>Cancel</Button>
+              <Button onClick={handleRoll}>
+                {isRolling ? t("Re-Roll") : t("Roll")}
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Paper>
       )}
       <style jsx>{`
