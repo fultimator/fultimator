8f797e617a4877583834481fbc789f837c43bb14
Acinoroc <acinoroc@gmail.com>
2024-07-10 13:16:15 +0200

    chore: keys

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAL0PZFyFcTg5OG7LE0wvBWGcpu1hsJ_Ro",
-  authDomain: "fabula-ultima-helper.firebaseapp.com",
-  projectId: "fabula-ultima-helper",
-  storageBucket: "fabula-ultima-helper.appspot.com",
-  messagingSenderId: "384266567864",
-  appId: "1:384266567864:web:3f56d8d7557cac54aef44d",
-  measurementId: "G-PN5NS6ZV35",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
